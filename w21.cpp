ccbbef9908e85bc44cda248730857d2897ee4710
shengyushen <shengyushen@gmail.com>
2018-10-24 23:36:55 +0800

    adding w21

--- w21.cpp
+++ w21.cpp
@@ -14,8 +14,10 @@ unsigned int char2ui(unsigned char c) {
 		return 10+(int)c-(int)'a';
 	} else if((int)c>=(int)'A' && (int)c<=(int)'Z') {
 		return 10+(int)c-(int)'A';
-	} else 
+	} else {
+//		printf("illegle char %02X\n",c);
 		assert(0);
+	}
 }
 
 unsigned char ui2char(unsigned int ui) {
@@ -23,69 +25,76 @@ unsigned char ui2char(unsigned int ui) {
 		return (unsigned char)((int)'0'+ui);
 	} else if(ui>=10 && ui<=15) {
 		return (unsigned char)((unsigned int)'a'+ui-10);
-	} else
-		assert(0);
-}
-
-struct  UnsignedShortArray{
-	long lLength;
-	unsigned char* pArray;
-};
-
-typedef struct UnsignedShortArray UnsignedShortArray_t;
-//converting two char to an unsigned int in one char position
-unsigned char TwoHex2UnsignedShort(unsigned char * p) {
-	//convert two char "1e" to 0x 1e
-	unsigned int high = char2ui(p[0]);
-	unsigned int low  = char2ui(p[1]);
-	return (unsigned char)(high*16+low);
-}
-
-UnsignedShortArray_t * HexString2ShortArray(unsigned char * p) {
-	long lLengthp=strlen((char*)p);
-	assert(lLengthp%2==0);
-	unsigned char *x = (unsigned char *)malloc(lLengthp/2);
-	for(long i=0;i<lLengthp/2;i++) {
-		x[i]=TwoHex2UnsignedShort(p+2*i);
+	} else {
+//		printf("illegle ui %x\n",ui);
+		assert(-1);
 	}
-	struct  UnsignedShortArray * pus= (struct UnsignedShortArray *)malloc(sizeof(struct UnsignedShortArray));
-	pus->lLength=lLengthp/2;
-	pus->pArray = x;
-	return pus;
 }
 
-void printShortArray(UnsignedShortArray_t * pus) {
-	printf("len=%d ",pus->lLength);
-	for(long i=0;i<pus->lLength;i++) {
-		printf("%02X",(unsigned int)(pus->pArray[i])%256);
+void chars2uis(unsigned char * p1, unsigned char * pres,long len) {
+	for(long i=0;i<len;i=i+2) {
+//		printf("high %02X %d\n",*(p1+i),i);
+//		printf("low  %02X %d\n",*(p1+i+1),i);
+		unsigned int high=char2ui(*(p1+i));
+		unsigned int low =char2ui(*(p1+i+1));
+		unsigned int res=high*16+low;
+		pres[i]=res;
 	}
-	printf("\n");
 }
 
-void freeShortArray(UnsignedShortArray_t * pus)  {
-	free(pus->pArray);
-	free(pus);
+void uis2chars(unsigned char * p1, unsigned char * pres, long len) {
+	for(long i=0;i<len;i++) {
+		printf("uis2chars %02X\n",p1[i]);
+		sprintf((char*)(pres+2*i),"%02X",p1[i]);
+	}
+}
+void strxor (unsigned char * p1,unsigned char * p2,unsigned char * pres, long len) {
+	for(long i=0;i<len;i++) {
+		pres[i]=p1[i]^p2[i];
+	}
+}
+
+unsigned int KEYSIZE=16;
+unsigned int KEYSIZECHAR=16*2;
+unsigned int MAXSIZE=2014;
+
+void cbc_aes(unsigned char * ct, unsigned char* key) {
+	printf("ct len %d \n",(int)strlen((char*)ct) );
+	printf("key len %d \n",(int)strlen((char*)key) );
+	assert(KEYSIZECHAR==(int)strlen((char*)key));
+	assert((int)strlen((char*)ct) % KEYSIZECHAR ==0);
+
+
+	//converting ct and key to uis
+	unsigned char ctui[MAXSIZE];
+	printf("ct change\n");
+	chars2uis(ct,ctui,strlen((char*)ct));
+	unsigned char keyui[MAXSIZE];
+	printf("key change\n");
+	chars2uis(key,keyui,strlen((char*)key));
+	// key bits so need to *8
+	AES_KEY aeskey;
+	AES_set_decrypt_key(keyui,KEYSIZE*8,&aeskey);
+
+	//although each key and frame is 16byte, each byte need two char to represented, so we need to move at 32 byte
+	unsigned char finalres[MAXSIZE] ;
+	for(long i=KEYSIZE;i<(int)strlen((char*)ct)/2;i=i+KEYSIZE) {
+		unsigned char dres[MAXSIZE];
+		AES_decrypt(ctui+i,dres,&aeskey);
+		unsigned char xorres[MAXSIZE];
+		strxor(ctui+i-KEYSIZE,dres,xorres,KEYSIZE);
+		uis2chars(xorres,finalres+i-KEYSIZE,KEYSIZE);
+		printf("hha\n");
+	}
+
+	finalres[strlen((char*)ct)-KEYSIZECHAR-1]=(unsigned char)0;
+	printf("finalres is : %s\n",finalres);
 }
 
 int main(int argc, char *argv[]) {
 	unsigned char ct[] = "4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81";
 	unsigned char key[] = "140b41b22a29beb4061bda66b6747e14";
-	
-	struct  UnsignedShortArray * pkeyus  = HexString2ShortArray(key);
-	printf("key in hex : \n");
-	printShortArray(pkeyus);
-
-	struct  UnsignedShortArray * pctus  = HexString2ShortArray(ct);
-	printf("ct in hex : \n");
-	printShortArray(pctus);
-
-	AES_KEY aeskey;
-	AES_set_decrypt_key(pkeyus->pArray,(pkeyus-lLength)*8,&aeskey);
-
-	for(long i=16;i<64;i=i+16) {
-		unsigned char dres[16];
-		AES_decrypt()
-	}
+	cbc_aes(ct,key);
 }
 
 
